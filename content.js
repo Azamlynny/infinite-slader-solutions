addbb17cd0a74ad0d9af060cd210cd4f18b02200
azamlynny <azamlynny@hotmail.com>
2021-04-30 10:07:18 -0400

    Update manifest to only work on Slader, add fetch

--- content.js
+++ content.js
@@ -1,1 +1,23 @@
-alert("Hello from A Coder")
+import fetch from 'node-fetch';
+
+const url = 'https://www.slader.com/textbook/9781118473504-elementary-linear-algebra-11th-edition/155/truefalse-exercises/i/';
+
+let header = new fetch.Headers();
+h.append(`X-Forwarded-For`, (Math.random().toString()));
+
+let req = new fetch.Request(url, {
+    method: 'GET',
+    headers: h,
+    mode: 'cors'
+});
+
+fetch(req)
+    .then( (response)=>{
+        if(response.ok){
+            return response.json();
+        }
+    })
+    .then( (jsonData) =>{
+       console.log(jsonData); 
+    });
+
