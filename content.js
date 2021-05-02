473b1d844102912d870cf54d3719fd2f5f02a5eb
azamlynny <azamlynny@hotmail.com>
2021-05-01 22:38:45 -0400

    Yesterday work backup

--- content.js
+++ content.js
@@ -1,23 +1,34 @@
-import fetch from 'node-fetch';
 
-const url = 'https://www.slader.com/textbook/9781118473504-elementary-linear-algebra-11th-edition/155/truefalse-exercises/i/';
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if( request.message === "clicked browser action" ) {
+        var firstHref = $("a[href^='http']").eq(0).attr("href");
+  
+        console.log(firstHref);
+      }
+    }
+  );
 
-let header = new fetch.Headers();
-h.append(`X-Forwarded-For`, (Math.random().toString()));
+// import fetch from 'node-fetch';
 
-let req = new fetch.Request(url, {
-    method: 'GET',
-    headers: h,
-    mode: 'cors'
-});
+// const url = 'https://www.slader.com/textbook/9781118473504-elementary-linear-algebra-11th-edition/155/truefalse-exercises/i/';
 
-fetch(req)
-    .then( (response)=>{
-        if(response.ok){
-            return response.json();
-        }
-    })
-    .then( (jsonData) =>{
-       console.log(jsonData); 
-    });
+// let header = new fetch.Headers();
+// h.append(`X-Forwarded-For`, (Math.random().toString()));
+
+// let req = new fetch.Request(url, {
+//     method: 'GET',
+//     headers: h,
+//     mode: 'cors'
+// });
+
+// fetch(req)
+//     .then( (response)=>{
+//         if(response.ok){
+//             return response.json();
+//         }
+//     })
+//     .then( (jsonData) =>{
+//        console.log(jsonData); 
+//     });
 
